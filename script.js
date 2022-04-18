a2d00eb75e8ad23e5398127f76b5a4531c61fcd8
Aman Tiwari <amantiwari@cedcommerce.com>
2022-04-18 15:49:21 +0530

    Completed

--- script.js
+++ script.js
@@ -1,136 +1,103 @@
-var taskInput=document.getElementById("new-task");
-var addButton=document.getElementsByTagName("button")[0];
-var incompleteTaskHolder=document.getElementById("incomplete-tasks");
-var completedTasksHolder=document.getElementById("completed-tasks");
+var todo = [];
+var completed_todo = [];
 
+function display(){
+    var list = ""
+    todo.forEach((element,index) => {
+    var item =`<li>
+    <input type="checkbox" onclick="check(${index})" /><label>${element}</label
+    ><input type="text" /><button class="edit" onclick="edit(${index})">Edit</button
+    ><button class="delete" onclick="del(${index})">Delete</button>
+    </li>`;
+    // console.log(index)
+    list += item;
+});
+document.getElementById("incomplete-tasks").innerHTML =list;
+};
 
+function add(){
+    var item  = document.getElementById("new-task").value;
+    todo.push(item)
+    console.log(todo)
+    display();
+};
 
-var createNewTaskElement=function(taskString){
+function edit(index){
+    var listitem = todo[index];
+    document.getElementById("todoindex").value=index;
+    var x = document.getElementById("todoindex").classList.add('todo');
+    document.getElementById("new-task").value=listitem;
+    document.getElementById('addbtn').style.display = 'none';
+    document.getElementById("updatebtn").style = display;
+};
 
-	var listItem=document.createElement("li");
-	var checkBox=document.createElement("input");
-	var label=document.createElement("label");
-	var editInput=document.createElement("input");
-	var editButton=document.createElement("button");
-	var deleteButton=document.createElement("button");
-	
-  label.innerText=taskString;
-
-
-	checkBox.type="checkbox";
-	editInput.type="text";
-
-	editButton.innerText="Edit";
-	editButton.className="edit";
-	deleteButton.innerText="Delete";
-	deleteButton.className="delete";
-
-
-
-
-	listItem.appendChild(checkBox);
-	listItem.appendChild(label);
-	listItem.appendChild(editInput);
-	listItem.appendChild(editButton);
-	listItem.appendChild(deleteButton);
-	return listItem;
+function update(){
+    var item = document.getElementById("new-task").value;
+    var index = document.getElementById("todoindex").value;
+    var x = document.getElementById("todoindex").className;
+    console.log(x);
+    if(x == "todo"){
+        todo[index]=item;
+        display();
+        document.getElementById("todoindex").className="";
+    }
+    else{
+        completed_todo[index]=item;
+        displaycomp();
+        document.getElementById("todoindex").className="";
+    }
+    
+    
+    document.getElementById('addbtn').style = display;
+    document.getElementById("updatebtn").style.display = 'none';
 }
 
+function del(index){
+    todo.splice(index,1);
+    display();
+};
 
+function check(index){
+    var value = todo[index];
+    completed_todo.push(value);
+    todo.splice(index,1);
+    displaycomp();
+    display();
+};
 
-var addTask=function(){
-	console.log("Add Task...");
-	
-	var listItem=createNewTaskElement(taskInput.value);
+// completed task
+function displaycomp(){
+    var list = ""
+    completed_todo.forEach((element,index) => {
+    var item =`<li>
+    <input type="checkbox" onclick="compcheck(${index})" /><label>${element}</label
+    ><input type="text" /><button class="edit" onclick="compedit(${index})">Edit</button
+    ><button class="delete" onclick="compdel(${index})">Delete</button>
+    </li>`;
+    console.log(index)
+    list += item;
+});
 
-	incompleteTaskHolder.appendChild(listItem);
-	bindTaskEvents(listItem, taskCompleted);
+document.getElementById("completed-tasks").innerHTML =list;
+};
 
-	taskInput.value="";
+function compedit(index){
+    var listitem = completed_todo[index];
+    document.getElementById("todoindex").value=index;
+    document.getElementById("new-task").value=listitem;
+    var x = document.getElementById("todoindex").classList.add('completed_todo')
+    document.getElementById('addbtn').style.display = 'none';
+    document.getElementById("updatebtn").style = display;
+};
+function compdel(index){
+    completed_todo.splice(index,1);
+    displaycomp();
+};
 
-}
-
-var editTask=function(){
-console.log("Edit Task...");
-console.log("Change 'edit' to 'save'");
-
-
-var listItem=this.parentNode;
-
-var editInput=listItem.querySelector('input[type=text]');
-var label=listItem.querySelector("label");
-var containsClass=listItem.classList.contains("editMode");
-
-		if(containsClass){
-
-		
-			label.innerText=editInput.value;
-		}else{
-			editInput.value=label.innerText;
-		}
-
-		listItem.classList.toggle("editMode");
-}
-
-
-var deleteTask=function(){
-		console.log("Delete Task...");
-
-		var listItem=this.parentNode;
-		var ul=listItem.parentNode;
-	
-		ul.removeChild(listItem);
-
-}
-
-var taskCompleted=function(){
-		console.log("Complete Task...");
-	
-	var listItem=this.parentNode;
-	completedTasksHolder.appendChild(listItem);
-				bindTaskEvents(listItem, taskIncomplete);
-
-}
-
-
-var taskIncomplete=function(){
-		console.log("Incomplete Task...");
-
-		var listItem=this.parentNode;
-	incompleteTaskHolder.appendChild(listItem);
-			bindTaskEvents(listItem,taskCompleted);
-}
-
-
-
-var ajaxRequest=function(){
-	console.log("AJAX Request");
-}
-
-addButton.onclick=addTask;
-addButton.addEventListener("click",addTask);
-addButton.addEventListener("click",ajaxRequest);
-
-
-var bindTaskEvents=function(taskListItem,checkBoxEventHandler){
-	console.log("bind list item events");
-
-	var checkBox=taskListItem.querySelector("input[type=checkbox]");
-	var editButton=taskListItem.querySelector("button.edit");
-	var deleteButton=taskListItem.querySelector("button.delete");
-
-			editButton.onclick=editTask;
-			
-			deleteButton.onclick=deleteTask;
-			checkBox.onchange=checkBoxEventHandler;
-}
-
-	for (var i=0; i<incompleteTaskHolder.children.length;i++){
-
-		bindTaskEvents(incompleteTaskHolder.children[i],taskCompleted);
-	}
-
-	for (var i=0; i<completedTasksHolder.children.length;i++){
-
-		bindTaskEvents(completedTasksHolder.children[i],taskIncomplete);
-	}
+function compcheck(index){
+    var value = completed_todo[index];
+    todo.push(value);
+    completed_todo.splice(index,1);
+    displaycomp();
+    display();
+};
